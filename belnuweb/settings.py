de11cd12249e0f74ecab280d59a02ef54cc17c4c
pmamone1 <69124285+pmamone1@users.noreply.github.com>
2022-09-22 16:39:58 -0300

    Update settings.py

--- belnuweb/settings.py
+++ belnuweb/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 PASSWORD_GMAIL = env('PASSWORD_GMAIL')
 
